3c567380e7cbb548502a241dc73ad31a7f770e34
brianbirir <brianbirir@gmail.com>
2020-01-01 22:23:05 +0300

    feat(module): add backup module & tests

---
+++ src/backup.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import datetime
+import zipfile
+
+
+class MissingDirectoryException(Exception):
+    """Raised when directory is missing"""
+    def __init__(self):
+        Exception.__init__(self, "The directory does not exist!")
+
+
+def get_file_paths(dir_name: str) -> list:
+    file_paths = []
+
+    if os.path.exists(dir_name):
+        # crawl directory and subdirectories
+        for root, directories, files in os.walk(dir_name):
+            for file_name in files:
+                full_file_path = os.path.join(root, file_name)
+                file_paths.append(full_file_path)
+        return file_paths
+    else:
+        raise MissingDirectoryException()
+
+
+def compress_backup(dir_name: str):
+    try:
+        if os.path.exists(dir_name):
+            print('Folder path exists')
+            compressed_file = f'drupalSiteBackup_{datetime.date.today().strftime("%Y%m%d")}.zip'
+            compressed_file_path = f'{dir_name}/{compressed_file}'
+
+            file_paths = get_file_paths(dir_name)
+            print('The following files will be compressed: ')
+            for file_name in file_paths:
+                print(file_name)
+
+            print('Writing files to a zip file')
+            with zipfile.ZipFile(compressed_file_path, 'w') as z:
+                for f in file_paths:
+                    z.write(f)
+            print('All files have been compressed successfully')
+            return True
+        else:
+            print('Folder path does not exist. Insert correct folder path')
+            return False
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        sys.exit()
---
+++ tests/test_backup.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import pytest
+import datetime
+from src.backup import get_file_paths, compress_backup, MissingDirectoryException
+
+TEMP_DIRECTORY = '/tmp/tempDir'
+
+
+@pytest.fixture()
+def initialize_folders(request):
+    os.mkdir(TEMP_DIRECTORY)
+    with open(f'{TEMP_DIRECTORY}/test_file.txt', 'w') as f:
+        f.write('I am a test file')
+
+    def delete_directory():
+        # delete temp directory and its contents
+        shutil.rmtree(TEMP_DIRECTORY)
+
+    request.addfinalizer(delete_directory)
+
+
+def test_get_file_paths_success(initialize_folders):
+    assert type(get_file_paths(TEMP_DIRECTORY)) is list
+
+
+def test_get_file_paths_failure():
+    with pytest.raises(MissingDirectoryException):
+        get_file_paths('/tmp/fakeDirectory')
+
+
+def test_compress_backup_success(initialize_folders):
+    assert compress_backup(TEMP_DIRECTORY)
+
+
+def test_compress_backup_failure():
+    assert compress_backup('/tmp/fakeDirectory') is False
+
+
+def test_compressed_file_exists(initialize_folders):
+    compress_backup(TEMP_DIRECTORY)
+    compressed_file = f'drupalSiteBackup_{datetime.date.today().strftime("%Y%m%d")}.zip'
+    assert os.path.exists(f'{TEMP_DIRECTORY}/{compressed_file}')
