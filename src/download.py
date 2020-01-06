3382ed47543c04f24d4de2404487d64c175ad25b
brianbirir <brianbirir@gmail.com>
2020-01-06 18:10:44 +0300

    feat(module): add download module

--- src/download.py
+++ src/download.py
@@ -1,9 +1,16 @@
-import sys
+import os
+import tarfile
+
 import requests
 
-
-DOWNLOAD_TMP = '/tmp'
+DOWNLOAD_TMP = '/tmp/drupal_update'
 DRUPAL_DOWNLOAD_BASE_URL = 'https://ftp.drupal.org/files/projects/drupal-'
+
+
+class MissingDirectoryException(Exception):
+    """Raised when directory is missing"""
+    def __init__(self):
+        Exception.__init__(self, "The directory does not exist!")
 
 
 class MissingVersion(Exception):
@@ -15,19 +22,45 @@ class MissingVersion(Exception):
 class InaccessibleDownloadLink(Exception):
     """Raised when Download link returns a response code other than 200"""
     def __init__(self):
-        Exception.__init__(self, "Response error while accessing Drupal download link!")
+        Exception.__init__(
+            self, "Response error while accessing Drupal download link!")
+
+
+class UnsuccessfulFileDownload(Exception):
+    """Raised when file download fails"""
+    def __init__(self):
+        Exception.__init__(self, "The download failed!")
+
+
+class WrongFileFormat(Exception):
+    """Raised when downloaded Drupal update file is wrong file format"""
+    def __init__(self):
+        Exception.__init__(self, "Downloaded file is wrong file format!")
+
+
+class FileExtractionFailed(Exception):
+    """Raised when extracting Drupal zip file fails"""
+    def __init__(self):
+        Exception.__init__(self, "Extraction of Zip file failed!")
+
+
+def ping_url(response: int) -> bool:
+    """Pings url"""
+    if response == 200:
+        return True
 
 
 def get_url(version: str) -> str:
+    """Gets and validates url"""
     # check if version is empty
     if not version:
         raise MissingVersion()
 
     try:
-        full_url = ''.join([DRUPAL_DOWNLOAD_BASE_URL, version, '.zip'])
-        ping_response = requests.get(full_url)
+        full_url = ''.join([DRUPAL_DOWNLOAD_BASE_URL, version, '.tar.gz'])
+        response_obj = requests.get(full_url)
 
-        if ping_url(ping_response.status_code):
+        if ping_url(response_obj.status_code):
             print('Drupal download link is accessible')
             return full_url
         else:
@@ -36,15 +69,51 @@ def get_url(version: str) -> str:
         print(str(e))
 
 
-def ping_url(response: int) -> bool:
-    if response == 200:
-        return True
+def check_download_file_type(drupal_file: str) -> bool:
+    return tarfile.is_tarfile(drupal_file)
 
 
-def download_drupal_update():
-    pass
+def download_drupal_update(version: str) -> bool:
+    """Downloads Drupal zip file via url"""
+    os.mkdir('/tmp/drupal_update')
+    try:
+        url = get_url(version)
+        response_obj = requests.get(url, stream=True)
+        drupal_update_file = f'{DOWNLOAD_TMP}/drupal-{version}.tar.gz'
+
+        with open(drupal_update_file, 'wb') as f:
+            f.write(response_obj.content)
+
+        print(f'Downloaded: {drupal_update_file}')
+        # uncompress_drupal_update(drupal_update_file, version)
+        if check_download_file_type(drupal_update_file):
+            return True
+        else:
+            print('Not a tar file!')
+
+    except UnsuccessfulFileDownload as e:
+        print(str(e))
+    except Exception as e:
+        print(str(e))
 
 
-def uncompress_drupal_update():
-    pass
+def uncompress_drupal_update(drupal_file: str, drupal_version: str):
+    print(drupal_file)
+    print(drupal_version)
+    """Extracts Drupal update tar file"""
+    try:
+        if tarfile.is_tarfile(drupal_file):
+            print('The file is a tar file, continuing with extraction!')
+            drupal_tar_file = tarfile.open(drupal_file, 'r')
+            drupal_tar_file.extractall(path=f'{DOWNLOAD_TMP}/')
+            drupal_tar_file.close()
 
+            if os.path.exists(f'{DOWNLOAD_TMP}/drupal-{drupal_version}/'):
+                print('File extraction successful')
+                return True
+            else:
+                raise MissingDirectoryException()
+        else:
+            print('Not a tar file!')
+    except Exception as e:
+        print(str(e))
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,27 +1,28 @@
 import os
+import time
 import shutil
-import pytest
-import requests
-from src.download import get_url, MissingVersion, InaccessibleDownloadLink
 
-# @pytest.fixture()
-# def initialize_folders(request):
-#     os.mkdir(TEMP_DIRECTORY)
-#     with open(f'{TEMP_DIRECTORY}/test_file.txt', 'w') as f:
-#         f.write('I am a test file')
-#
-#     def delete_directory():
-#         # delete temp directory and its contents
-#         shutil.rmtree(TEMP_DIRECTORY)
-#
-#     request.addfinalizer(delete_directory)
+import pytest
+
+from src.download import get_url, MissingVersion, InaccessibleDownloadLink, download_drupal_update, uncompress_drupal_update
+
+VERSION = '7.69'
+DRUPAL_UPDATE_FILE = f'/tmp/drupal_update/drupal-{VERSION}.tar.gz'
+
+
+@pytest.fixture()
+def initialize_folders(request):
+    def delete_drupal_update_file():
+        os.remove(DRUPAL_UPDATE_FILE)
+        shutil.rmtree('/tmp/drupal_update')
+
+    request.addfinalizer(delete_drupal_update_file)
 
 
 def test_get_url_success():
-    version = '7.69'
     assert get_url(
-        version
-    ) == f'https://ftp.drupal.org/files/projects/drupal-{version}.zip'
+        VERSION
+    ) == f'https://ftp.drupal.org/files/projects/drupal-{VERSION}.tar.gz'
 
 
 def test_missing_version():
@@ -34,3 +35,13 @@ def test_url_response_error():
     version = '1.777'
     with pytest.raises(InaccessibleDownloadLink):
         get_url(version)
+
+
+def test_download_success(initialize_folders):
+    assert download_drupal_update(VERSION)
+
+
+def test_compressed_file_extraction(initialize_folders):
+    download_drupal_update(VERSION)
+    time.sleep(3)
+    assert uncompress_drupal_update(DRUPAL_UPDATE_FILE, VERSION)
